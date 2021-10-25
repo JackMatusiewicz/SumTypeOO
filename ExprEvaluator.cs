b1647b22ff22d743a16b61b7d7b36baf467c1c6b
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2021-10-25 17:11:09 +0100

    Adding evaluator

---
+++ ExprEvaluator.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace SumTypeOO
+{
+    public sealed class ExprEvaluator<A> : IExprVisitor<A, A>
+    {
+        private record ExprMapEvaluator()
+            : IMapVisitor<A, A>
+        {
+            // TODO - could cache based on type rather than repeatedly making objects.
+            public A Visit<B>(Func<B, A> f, IExprElement<B> v)
+            {
+                var childEvaluator = new ExprEvaluator<B>();
+                var b = v.Accept(childEvaluator);
+                return f(b);
+            }
+        }
+
+        private record ExprApplyEvaluator()
+            : IExprApplyVisitor<A, A>
+        {
+            public A Visit<B> (
+                IExprElement<Func<B,A>> f,
+                IExprElement<B> v)
+                {
+                    var func = f.Accept(new ExprEvaluator<Func<B, A>>());
+                    var value = v.Accept(new ExprEvaluator<B>());
+                    return func(value);
+                }
+        }
+
+        public A Visit(ExprValue<A> v) => v.Value;
+
+        public A Visit(ExprMap<A> v) =>
+            v.Accept(new ExprMapEvaluator());
+
+        public A Visit(ExprApply<A> v) =>
+            v.Accept(new ExprApplyEvaluator());
+    } 
+}
